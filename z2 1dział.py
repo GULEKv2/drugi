2424302c397a6e963bc887931fb34663d04daa7c
GULEKv2 <97672904+GULEKv2@users.noreply.github.com>
2022-01-29 00:10:37 +0100

    Update z2 1dział.py

--- z2 1dział.py
+++ z2 1dział.py
@@ -1,5 +1,6 @@
-x = 1
-if x < 100:
-    print("pierwszy warunek został spełniony")
-else:
-    print("drugi warunek został spełniony")
+def tak(x):
+    x = 1
+    if x < 100:
+        return print("pierwszy warunek został spełniony")
+    else:
+        return print("drugi warunek został spełniony")
